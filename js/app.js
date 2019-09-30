c30784ad368a08cc078c28f006377bf2d681df66
Collin McMahon <collin@Collins-MacBook-Pro.local>
2019-09-30 14:11:33 -0400

    Mobile Menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+$(document).foundation()
+// hide the overlay initially
+$('.overlay').hide();
 
+// create a button to close the overlay
+// add it to the overlay
+$('.overlay').append('<button>X</button>');
+
+// open overlay
+$('main p').click(function(){
+  $('.overlay').show();
+});
+
+// close overlay
+$('.overlay').click(function(){
+  $('.overlay').hide();
+});
