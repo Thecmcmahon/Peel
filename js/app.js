1cae085c279cd7938e2b7955d0f4241110aa7891
Collin McMahon <collin@Collins-MacBook-Pro.local>
2019-09-30 15:16:44 -0400

    mobile menu

--- js/app.js
+++ js/app.js
@@ -2,9 +2,6 @@ $(document).foundation()
 // hide the overlay initially
 $('.overlay').hide();
 
-// create a button to close the overlay
-// add it to the overlay
-$('.overlay').append('<button>X</button>');
 
 // open overlay
 $('main img').click(function(){
