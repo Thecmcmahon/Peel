e2ce818d2cdaca9698d7b0f3875522b6487a5852
Collin McMahon <collin@Collins-MacBook-Pro.local>
2019-11-04 19:01:42 -0500

    recipes

--- js/app.js
+++ js/app.js
@@ -13,3 +13,26 @@ $(function() {
 });
 
 
+$(function() {
+
+  $(window).scroll(function() {
+
+    var mass = Math.max(250, 2400-1*$(this).scrollTop());
+
+    $('#vari2').css({'font-variation-settings': '"wght"' + mass});
+    
+
+  });
+});
+
+$(function() {
+
+  $(window).scroll(function() {
+
+    var mass = Math.max(250, 3200-1*$(this).scrollTop());
+
+    $('#vari3').css({'font-variation-settings': '"wght"' + mass});
+    
+
+  });
+});
