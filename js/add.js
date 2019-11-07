fa68089b9bbdb8e89cd273bcc7f84454f7dc18f1
Collin McMahon <collin@Collins-MacBook-Pro.local>
2019-11-07 17:53:39 -0500

    slider working

--- js/add.js
+++
@@ -1,6 +0,0 @@
-function function_one() {
-	sleep(1000);
-    document.getElementById("image").innerHTML = "<img src='images/slider/peel " + "100" + ".jpg'>";
-
-}
-
--- js/add2.js
+++
@@ -1,5 +0,0 @@
-function function_two() {
-	sleep(1000);
-    document.getElementById("image").innerHTML = "<img src='images/slider/peel " + "150" + ".jpg'>";
-    function_one();
-}
